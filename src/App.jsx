522c8c8cf7d78c46b54641ea8ffc7b0cc529e91f
Juan Felipe Obregon Rodriguez <juanfelipeobregonrodriguez@gmail.com>
2021-04-28 18:23:25 -0500

    router

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {
+  BroweserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom'
+
+ function App() {
+  return (
+    <Router>
+     <Switch>
+       <Route></Route>
+     </Switch>
+
+    </Router>
+  )
+}
+export default App;
