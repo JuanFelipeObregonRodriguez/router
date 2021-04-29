6c2301a84a7b6bcedca87bf7d26b9f8593d38574
Juan Felipe Obregon Rodriguez <juanfelipeobregonrodriguez@gmail.com>
2021-04-28 21:31:47 -0500

    useEffect

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 
 const Base = () => {
+     const [nombre, setNombre] = useState('Juan')
+    useEffect( ()=>{
+
+      setTimeout(()=>{
+
+              setNombre('Felipe')
+      }, 3000)
+
+    })
     return (
         <div>
             <h1>pagina de base ruta/</h1>
+            {nombre}
         </div>
     )
 }
